8b42765141a8f2f05d0f1e51e62ef94ca25852e9
Pony <pony076152340@gmail.com>
2023-09-21 17:44:30 +0800

    Add unstyled component to index page

--- pages/index/page.tsx
+++ pages/index/page.tsx
@@ -1,7 +1,24 @@
 export function Component() {
   return (
     <div>
-      FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+      <h1>Online Typing Practice for Programmers</h1>
+      <div>
+        <img src={"/screenshot.png"} width={560} />
+        <div>
+          <p>support language</p>
+          <ul>
+            <li>C</li>
+            <li>C++</li>
+            <li>C#</li>
+            <li>Go</li>
+            <li>Java</li>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>R</li>
+          </ul>
+        </div>
+      </div>
+      <h2>Don't Just be a Coder, be a SpeedCoder!</h2>
     </div>
   );
 }
