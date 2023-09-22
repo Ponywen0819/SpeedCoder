b2e58625dc715d404fe9f75d2dddb513970cb78b
Pony <pony076152340@gmail.com>
2023-09-22 17:14:31 +0800

    Select language navigation

--- pages/type/component/score/language.tsx
+++ pages/type/component/score/language.tsx
@@ -1,9 +1,45 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import type { ChangeEvent } from "react";
+
 export const Language = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const language = location.pathname.split("/")[2];
+  console.log(language);
+
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    navigate(`/type/${value}`);
+  };
+
   return (
     <div className="language-container">
       <p className="label">當前語言</p>
-      <select className="language-select">
-        <option className="language-option">Javascrpit</option>
+      <select className="language-select" onChange={onChange} value={language}>
+        <option className="language-option" value={"c"}>
+          C
+        </option>
+        <option className="language-option" value={"cpp"}>
+          C++
+        </option>
+        <option className="language-option" value={"cs"}>
+          C#
+        </option>
+        <option className="language-option" value={"go"}>
+          GO
+        </option>
+        <option className="language-option" value={"java"}>
+          Java
+        </option>
+        <option className="language-option" value={"js"}>
+          JavaScript
+        </option>
+        <option className="language-option" value={"py"}>
+          Python
+        </option>
+        <option className="language-option" value={"r"}>
+          R
+        </option>
       </select>
     </div>
   );
